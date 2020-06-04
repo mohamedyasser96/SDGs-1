f9714dc7fe7e79e0ff45ca9c32ae37c4f3b36437
karimmady <karimmady5@hotmail.com>
2020-06-04 10:15:56 +0100

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
+  {
+    path:'',
+    component:LandingPageComponent
+  }
 ];
 
 @NgModule({
